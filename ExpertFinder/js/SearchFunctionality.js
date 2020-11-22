1d0498b868e7e1a3c5fa57f3101cb7667b648fb7
Daniel <felixda@oregonstate.edu>
2020-11-22 18:07:49 -0500

    Added Git modal

--- ExpertFinder/js/SearchFunctionality.js
+++ ExpertFinder/js/SearchFunctionality.js
@@ -101,3 +101,44 @@ expertList = exampleExperts;
 listNames(expertList);
 showProfile(expertList,0);
 
+var request = new XMLHttpRequest();
+        request.open('GET','https://api.github.com/users/kevinlichang/repos' , 
+        true)
+        request.onload = function() {
+            var data = JSON.parse(this.response);
+            console.log(data);
+            var statusHTML = '';
+            $.each(data, function(i, status){
+                statusHTML += '<div class="card"> \
+                                    <a href= '+status.html_url+'> ' + status.name +  ' </a> \
+                                </div>';
+            });
+            $('.repos').html(statusHTML);
+        }
+        request.send();
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
