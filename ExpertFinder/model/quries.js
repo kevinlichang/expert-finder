413ee26b33bec68eb2f27fc4f76f8a46a83d9359
clarjohn <59753565+clarjohn@users.noreply.github.com>
2020-11-22 15:22:36 -0800

    Account get

--- ExpertFinder/model/quries.js
+++ ExpertFinder/model/quries.js
@@ -2,3 +2,29 @@ const e = require("express");
 const express = require("express")
 
 queriesRouter = express.Router();
+
+
+// Account get 
+queriesRouter.get('/account', (req,res,next)=>{
+    let username = req.query.username;
+    let password = req.query.password;
+    let accountid = req.query.accountid;
+    let values = []
+    let sql  = 'select username, id, accountactive from account_info '
+    if((password != null) && (username != null)){
+      sql += 'where username = ?'
+      sql += 'and userpassword = ?'
+      values.push(username);
+      values.push(password);
+    }else if((accountid != null)){
+      sql += 'where id = ?'
+      values.push(accountid);
+    }
+    db.all(sql,values,(err,rows)=>{
+      if(err){
+        res.status(500).json({"error": err.message});
+      }else{
+          res.status(200).json({accounts: rows});
+      }
+    });
+  });
