56d3b900ec6be63d53469ad7da98e5e3d6ec5db2
changkev <38297595+kevinlichang@users.noreply.github.com>
2020-11-22 22:08:33 -0500

    Update emailConfirmScript.js

--- ExpertFinder/js/emailConfirmScript.js
+++ ExpertFinder/js/emailConfirmScript.js
@@ -3,12 +3,13 @@ var confirmURL = "http://flip3.engr.oregonstate.edu:4001/confirm-profile.html"
 
 $(document).ready(function(){
   var from,to,subject,text;
-  $("#register-submit").click(function(){     
+  $("#register-submit").click(function(){    
     to=$("#email").val();
-
     subject = "Confirm Profile for " + $("#firstname").val();
 
-    text = "A new profile has been registered for " + $("#firstname").val() + " " + $("#lastname").val() + ". Please click here to confirm: " + confirmURL + "?email=" + $("#email").val();
+    var fullName = $("#firstname").val() + " " + $("#lastname").val();
+    var confirmProfileLink = confirmURL + "?email=" + $("#email").val();
+    text = "A new profile has been registered for " + fullName + ". Please click here to confirm: " + confirmProfileLink;
     
     $.get(url + "send-email-confirm",
     {
