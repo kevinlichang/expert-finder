d22cc2f331d11aa7d13bd587a706a07170b0723d
Brandon Metcalf <52182016+bmetcalf21@users.noreply.github.com>
2020-11-29 23:11:19 -0800

    Minor changes to app.js

--- ExpertFinder/app.js
+++ ExpertFinder/app.js
@@ -1,6 +1,6 @@
 //create express instance
 const express = require('express');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var path = require('path');
 const bodyParser = require('body-parser');
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 4001;
 const { initDB } = require('./models/db.js');
 global.db = initDB();
 
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(session({
   secret: 'mySuperDuperSecret789BlahBlah',
   resave: true,
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
   extended: true,
 })
 )
-app.use(express.static(__dirname + '/'))
+app.use(express.static(__dirname + '/'));
 
 
 // load queries
@@ -40,18 +40,18 @@ const getAccount = (req, res) => {
 }
 
 
-// Starting Page
-app.get('/', (req, res) => {  
- 
-  res.sendFile(path.join(__dirname + '/index.html'));  
+// // Starting Page
+// app.get('/', (req, res) => {  
+//   console.log('Hi this is a test');
+//   res.sendFile(path.join(__dirname + '/index.html'));  
 
-});
+// });
 
 
-///test db connection and server connection, Should now only load if index.html isn't found
-app.get('/', (req, res, next) => {
-  res.send('Hello World!')
-});
+// ///test db connection and server connection, Should now only load if index.html isn't found
+// app.get('/', (req, res, next) => {
+//   res.send('Hello World!')
+// });
 
 
 
@@ -62,7 +62,7 @@ app.use('/queries', queriesRouter);
 
 
 // Login
-
+var sessID;
 app.post('/login', (req, res) => {
   let username = req.body.username;
   let userpassword = req.body.userpassword;
@@ -73,21 +73,29 @@ app.post('/login', (req, res) => {
       if (rows.length > 0) {
         req.session.loggedin = true;
         req.session.username = username;
-        
+
         // Store the userID from the current matching row as the session ID
         req.session.ID = rows[0].id        
-        console.log('TEST A');        
-        res.redirect('/');          
+        console.log('Username and Password match found');   
+        console.log(req.cookies);
+        console.log('Current userID is: ' + req.session.ID);   
+        sessID = req.session.ID;          
+                
       } else {
-        console.log('TEST B');
+        console.log('No username and password match found');
         res.send('Incorrect Username and/or Password.');
       }
       
     })
   } else {
-    console.log('TEST C');
+    console.log('Username and/or password not entered');
     res.send('Please enter Username and Password.');    
   }
+});
+
+// Get sessionID json
+app.get('/sessionID', (req, res) => {
+  res.json({sessID: req.session.ID});  
 });
 
 
