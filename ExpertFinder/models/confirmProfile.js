c4b6a7c1ecd69ff703f8f8c69be964910ffb63b2
changkev <38297595+kevinlichang@users.noreply.github.com>
2020-12-02 02:36:00 -0500

    Update confirmProfile.js

--- ExpertFinder/models/confirmProfile.js
+++ ExpertFinder/models/confirmProfile.js
@@ -13,17 +13,23 @@ confirmPageRouter.get('/confirm-profile', function(req, res) {
   
   let sql = 'select * from user_info where email = ?'
   
-  db.get(sql, email, (err, rows) => {
+  db.get(sql, email, (err, data) => {
     if (err) {
       res.status(500).json({ "error": err.message });
     } else {
-      console.log(rows)
+      var context = {
+        results: data
+      };
+      console.log(context.results)
+
+
+      res.render('confirm-profile', context);
     }
   })
 
 
 
-  res.render('confirm-profile');
+  
 
 
 });
